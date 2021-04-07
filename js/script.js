d1ff751e893278423d22facf7e06fc37b51e83a5
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-07 16:24:18 +0200

    Exercise redone - cicle for deleted, only if else and bonus

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
                        //aggiungere un timer che permetta di ciclare le immagini in automatico (cambio ogni 3sec)
 
 function myNewVue() {
+
      new Vue({
           el : '#js-vue',
+
           data : {
                namesArray : ['judy', 'panam', 'rogue', 'johnny'],
                starterRoad: 'img/',
@@ -11,32 +13,29 @@ function myNewVue() {
                finalRoad: '.jpg',
                switchCount: 0
           },
+
           mounted: function() {
                setInterval(this.next, 3000);
           },
-          methods: {
 
+          methods: {
                next: function() {
                     this.switchCount++;
-                    for (let i = 0; i < this.namesArray.length; i++) {
-                         if( i == this.switchCount ) {
-                              this.name = this.namesArray[i];
-                         } else if ( this.switchCount == this.namesArray.length) {
-                              this.switchCount = 0;
-                              this.name = this.namesArray[this.switchCount];
-                         }
+                    if ( this.switchCount == this.namesArray.length) {
+                         this.switchCount = 0;
+                         this.name = this.namesArray[this.switchCount];
+                    }else {
+                         this.name = this.namesArray[this.switchCount];
                     }
                },
 
                prev: function() {
                     this.switchCount--;
-                    for (let i = 0; i < this.namesArray.length; i++) {
-                         if( i == this.switchCount) {
-                              this.name = this.namesArray[i];
-                         } else if ( this.switchCount < 0) {
-                              this.switchCount = this.namesArray.length - 1;
-                              this.name = this.namesArray[this.switchCount];
-                         }
+                    if ( this.switchCount < 0) {
+                         this.switchCount = this.namesArray.length - 1;
+                         this.name = this.namesArray[this.switchCount];
+                    } else {
+                         this.name = this.namesArray[this.switchCount];
                     }
                }
           }
