6efcfacb1db5c9a450f5c3c6a41f806e59e41403
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-07 15:38:41 +0200

    next prev done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-                       //Recuperando quanto visto in classe, generare il js per lo slider, con l'obbiettivo di comprendere cosa sta succedendo.
+                       //Ricreare lo slider visto in jquery, con tecnologia Vue.
+
+function myNewVue() {
+     new Vue({
+          el : '#js-vue',
+          data : {
+               nameArray : ['judy', 'panam', 'rogue', 'johnny'],
+               starterRoad: 'img/',
+               name: 'judy',
+               finalRoad: '.jpg',
+               nameCount: 0
+          },
+          methods: {
+
+               next: function() {
+                    this.nameCount++;
+                    for (let i = 0; i < this.nameArray.length; i++) {
+                         if( i == this.nameCount ) {
+                              console.log(i, this.nameCount);
+                              this.name = this.nameArray[i];
+                         } 
+                    }
+               },
+
+               prev: function() {
+                    this.nameCount--;
+                    for (let i = 0; i < this.nameArray.length; i++) {
+                         if( i == this.nameCount) {
+                              this.name = this.nameArray[i];
+                         }
+                    }
+               }
+          }
+     })
+}
+
+$(document).ready(myNewVue);
